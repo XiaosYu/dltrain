5f9fe84d487856b4f7c4e3b04b7d4e5336934a6e
XiaosYu <103820358+XiaosYu@users.noreply.github.com>
2024-05-07 12:11:53 +0800

    Delete dltrain-demo directory

--- dltrain-demo/src/dltrain/builder/core.py
+++
@@ -1,6 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-class Wizard(metaclass=ABCMeta):
-    def get_kwargs(self):
-        kwargs = {key.lstrip('_'): value for (key, value) in self.__dict__.items()}
-        return kwargs
